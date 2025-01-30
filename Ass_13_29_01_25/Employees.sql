4f91c20eaad721c713e0d5693aa1161c112a0cea
Sanchit Patil <Sanchit.Patil@wissen.com>
2025-01-30 10:06:42 +0530

    29/1 done

--- Ass_13_29_01_25/Employees.sql
+++ Ass_13_29_01_25/Employees.sql
@@ -1,10 +1,10 @@
 create table EMPLOYEE(
 	eid SERIAL PRIMARY KEY,
 	ename varchar(20) not null,
-	eage SMALLINT NOT NULL CHECK (AGE > 20 AND AGE <= 60),
+	eage SMALLINT NOT NULL CHECK (eage > 20 AND eage <= 60),
 	esalary DECIMAL(10, 2) not null,
 	edesignation varchar(10) not null,
 	edepartment varchar(10) not null
 );
 
-drop 
+select * from EMPLOYEE;

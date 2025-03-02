5625c4a3f8b876cd932da9b1d4dcb3b6659b6f72
Sanchit Patil <137409077+sanch-18@users.noreply.github.com>
2025-03-03 00:02:33 +0530

    Add files via upload

--- JS_Services/script.js
+++ JS_Services/script.js
@@ -4,87 +4,98 @@ const loanComponentClickable = document.getElementById('loan-click');
 const depositComponent = document.getElementById('deposit');
 const depositComponentClickable = document.getElementById('deposit-click');
 
-const loanForm = `
-<form class="login-form">
-    <div class="form-group">
-        <label for="netbanking-id">Applicant's Full name</label>
-        <input type="text" id="full-name" name="full-name" required>
-    </div>
-    <div class="form-group">
-        <label for="loan-type">Type of Loan</label>
-        <select id="loan-type" class="type-of-loan" name="loan-type" required>
-            <option value="" disabled selected>Select a Loan Type</option>
-            <option value="home">Home Loan</option>
-            <option value="car">Auto Loan</option>
-            <option value="personal">Personal Loan</option>
-        </select>
-    </div>    
-    <script>
-        const loanType = document.querySelector('.type-of-loan');
-        const hiddenComponent = document.querySelector('.hidden');
+const loanType = document.querySelector('.type-of-loan');
+const hiddenComponent = document.querySelectorAll('.hidden');
 
-        document.write("Hello ");
+const paraLoanComponent = document.getElementById("para");
+const formLoanComponent = document.getElementById("form-content");
 
-        loanType.addEventListener("change", () => {
-            var value = loanType.value;  
-            if (value) {
-                hiddenComponent.style.display = "block"; 
-            } else {
-                hiddenComponent.style.display = "none";
-            }
-        });
-    </script>
-    <div class="form-group, hidden">
-        <label for="password">Interest</label>
-        <span></span>
-    </div>
-    <div class="form-group, hidden">
-        <label for="loan-type">Tenure</label>
-        <select id="loan-type" name="loan-type" required>
-            <option value="home">Home Loan</option>
-            <option value="car">Auto Loan</option>
-            <option value="personal">Personal Loan</option>
-        </select>
-    </div>
-    <div class="form-group, hidden">
-        <label for="netbanking-id">Amount</label>
-        <input type="number" id="amount" name="amount" min="18" max="100" required>
-    </div>
-    
-    <div class="form-group">
-        <button type="submit">calculate</button>
-    </div>
-</form>
-`;
-
-const loanPara = `
-<p>We offer a range of loan products designed to meet your personal financial needs, including home loans,
-    auto loans, and more.</p>
-<p>Whether you're planning for a major purchase or need some extra cash, our loans are flexible to suit your
-    requirements.</p>
-`;
-
-const depositForm = ``;
-const depositPara = ``;
-
-const forms = [loanForm, depositForm];
-const paras = [loanPara, depositPara];
+const paraDepositComponent = document.getElementById("para-deposit");
+const formDepositComponent = document.getElementById("form-content-deposit");
 
 var flags = [false, false];
+let r = 12;
 
-function display(component, flag) {
-    if (flags[flag]) {
-        component.innerHTML = paras[flag];  
+function displayLoan() {
+    if (flags[0]) {
+        paraLoanComponent.style.display = "none";
+        formLoanComponent.style.display = "block";
     } else {
-        component.innerHTML = forms[flag]; 
+        paraLoanComponent.style.display = "block";
+        formLoanComponent.style.display = "none";
     }
-    flags[flag] = !flags[flag];  
+    flags[0] = !flags[0];
 }
 
-loanComponentClickable.addEventListener('click', function() {
-    display(loanComponent, 0);  
+function displayDeposit() {
+    if (flags[1]) {
+        paraDepositComponent.style.display = "none";
+        formDepositComponent.style.display = "block";
+    } else {
+        paraDepositComponent.style.display = "block";
+        formDepositComponent.style.display = "none";
+    }
+    flags[1] = !flags[1];
+}
+
+document.querySelector(".login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var tenure_value = parseInt(document.forms["login-form"]["tenure"].value);
+    var amt_value = parseInt(document.forms["login-form"]["amount"].value);
+    var r_value= (r/(12*100));
+    var val = (1+r_value)**tenure_value;
+    var emi = Math.round((r_value*amt_value*val)/(val-1));
+    document.getElementById('emi-display').style.display = "block";
+    document.getElementById('emi').innerText = "Rs. "+emi;
 });
 
-depositComponentClickable.addEventListener('click', function() {
-    display(depositComponent, 1); 
+document.querySelector(".login-form-deposit").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var tenure_value = parseInt(document.forms["login-form-deposit"]["tenure"].value);
+    var amt_value = parseInt(document.forms["login-form-deposit"]["amount"].value);
+    var r_value= 7;
+    var maturity_value = 34000;
+    document.getElementById('maturity-display').style.display = "block";
+    document.getElementById('maturity').innerText = "Rs. "+maturity_value;
+});
+
+loanComponentClickable.addEventListener('click', function () {
+    displayLoan();
+});
+
+depositComponentClickable.addEventListener('click', function () {
+    displayDeposit();
+});
+
+
+loanType.addEventListener("change", () => {
+    var value = loanType.value;
+    if (value) {
+        hiddenComponent.forEach(element => {
+            element.style.display = 'block'; 
+        });
+        switch (value) {
+            case "home": 
+                r = 9
+                var minamt = 500000, maxamt = 10000000, maxtenure=30;       
+                break;
+            case "car":
+                r = 11;
+                var minamt = 100000, maxamt = 1500000, maxtenure=7; 
+                break;
+            case "personal":
+                r = 15;
+                var minamt = 10000, maxamt = 500000, maxtenure=5; 
+                break;
+        }        
+        rateComponent = document.getElementById("rate-of-interest");
+        tenureComponent = document.getElementById("tenure");
+        amountComponent = document.getElementById("amount");
+        rateComponent.innerText = r+"%";
+        tenureComponent.placeholder = "Must be between 0-"+maxtenure+" years";
+        tenureComponent.max = maxtenure;
+        amountComponent.min = minamt;
+        amountComponent.maxamt = maxamt;
+        amountComponent.placeholder = "Must be between "+minamt+" - "+maxamt+" rupees";    
+    }
 });
